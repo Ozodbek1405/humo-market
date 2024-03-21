1845c0bdc7c7d7f953bd2d6e3bdf8241780863fa
Ozodbek <roziyevozodbek75@gmail.com>
2024-03-21 16:43:11 +0500

    master change

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') | HUMO MARKET</title>
-    <meta name="description" content="Ulgurji savdo">
+    <meta name="description" content="humo market">
     <meta name="author" content="Ro`ziqulov Baxtiyor">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="icon" type="image/jpg" href="{{asset('images/icons/seo_icon.jpg')}}" />
